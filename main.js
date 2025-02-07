9752e30ab1c3246b8ee174bceda3971f800bb876
Mickey <mickeyrossgame@gmail.com>
2025-02-07 11:15:49 +0200

    burgerMenu

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 document.getElementById("hamburger__button").addEventListener("click", function () {
-    document.getElementById("navigation__mobile").style.display = "flex";
+    let checkElement = document.getElementById("navigation__mobile");
+    let targetElement = document.getElementById("hamburger__button");
+
+    if (window.getComputedStyle(checkElement).display === "none") {
+        targetElement.classList.add("active-nav-mobile");
+    } else {
+        targetElement.classList.remove("active-nav-mobile");
+    }
+});
+
+let blockScroll = document.body;
+
+document.getElementById("hamburger__button").addEventListener("click", function () {
+    let target = document.getElementById("navigation__mobile");
+
+    if (target.style.display === "flex") {
+        target.style.display = "none";
+        blockScroll.style.removeProperty("overflow-y");
+    } else {
+        target.style.display = "flex";
+        blockScroll.style.overflowY = "hidden";
+    }
 });
 
 document.getElementById("close__button-nav-mobile").addEventListener("click", function () {
     document.getElementById("navigation__mobile").style.display = "none";
+    document.getElementById("hamburger__button").classList.remove("active-nav-mobile");
+    blockScroll.style.removeProperty("overflow-y");
 });
